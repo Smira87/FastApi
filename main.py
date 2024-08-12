9b55772ab8da9172a4a38c3b6f705f539af05fc9
Serhii Smirnov <snoff87@gmail.com>
2024-08-12 21:29:17 +0300

    Add tasks func in main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Annotated
 
 app = FastAPI()
 
-class Task(BaseModel):
+class STaskAdd(BaseModel):
     name: str
     description: Optional[str] = None
 
-@app.get('/home')
-def get_tasks():
-    task = Task(name = "Learn Python")
-    return {"data": task}
+class STaskGet(STaskAdd):
+    id: int
+
+tasks = []
+@app.post("/tasks")
+async def add_task(
+        task: Annotated[STaskAdd, Depends()]
+):
+    tasks.append(task)
+    return {"OK": True}
+
+# @app.get('/home')
+# def get_tasks():
+#     task = Task(name = "Learn Python")
+#     return {"data": task}
