8ae8f81c9165553d1ec67335763d3c7be8574b86
Serhii Smirnov <snoff87@gmail.com>
2024-08-12 21:12:37 +0300

    Add some text to main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+class Task(BaseModel):
+    name: str
+    description: Optional[str] = None
+
 @app.get('/home')
-def get_home():
-    return {"data": "Hello World"}
+def get_tasks():
+    task = Task(name = "Learn Python")
+    return {"data": task}
